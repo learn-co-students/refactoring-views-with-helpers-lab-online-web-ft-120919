70da3a0fcb53f16cc32efd45f5e437dc418adfff
allison_johnson <allison.cuttler@gmail.com>
2020-03-05 19:06:09 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,14 @@
 module SongsHelper
 
+  def display_artist(song)
+    html = ""
+    if song.artist 
+      html = "<a href='/artists/#{song.artist.id}'>#{song.artist_name}</a>"
+      #html << "link_to #{song.artist_name}, artist_path(song.artist)"
+    else
+      html = "<a href='/songs/#{song.id}/edit'>Add Artist</a>"
+    end #if
+    html.html_safe
+  end #display_artist
+
 end
