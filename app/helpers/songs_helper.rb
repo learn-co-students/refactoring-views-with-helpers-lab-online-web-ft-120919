ed92ece099f645a2bbb4cf095b82d0aa4b02f9f9
github username <meliajm@gmail.com>
2020-03-06 20:23:42 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,14 +1,13 @@
 module SongsHelper
 
     def display_artist(song)
-        if song.artist.empty?
+        if song.artist.nil?
             #artist edit page link_to
-            link_to "Add Artist", edit_artist_path(song.artist)
+            link_to "Add Artist", edit_song_path(song)
         else
             #artist show page link_to
              link_to song.artist_name, artist_path(song.artist)
         end
-
     end
 
 end
