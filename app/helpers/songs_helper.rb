9665c91aa8a60958e822ea92245a20b1263aa3d2
github username <meliajm@gmail.com>
2020-03-06 19:55:48 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,14 @@
 module SongsHelper
 
+    def display_artist(song)
+        if song.artist.blank?
+            #artist edit page link_to
+            link_to "Add Artist", edit_artist_path(song.artist)
+        else
+            #artist show page link_to
+             link_to song.artist_name, artist_path(song.artist)
+        end
+
+    end
+
 end
