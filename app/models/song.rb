e2618582bfa1de109ae6927a274f697899b0c5e6
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-05-05 18:23:41 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,22 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
   end
 
   def artist_name=(name)
+
+    if self.artist
+      self.artist.name = name
+    else
+      artist = Artist.find_or_create_by(name: name)
+      artist.songs << self
+    end
+
   end
+  
 end
